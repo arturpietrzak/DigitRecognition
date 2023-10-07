2806fdf3b6eaeb75be3369683f5b362bdeb936e9
arturpietrzak <artur.pietrzak1@outlook.com>
2023-10-08 00:14:36 +0200

    Update file

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, send_from_directory
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('dist/index.html')
+    return send_from_directory('./dist/index.html')
