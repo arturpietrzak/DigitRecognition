72f3f3eb3b248b375c087a77cfc7d3a1451c2b79
arturpietrzak <artur.pietrzak1@outlook.com>
2023-10-08 01:20:02 +0200

    Fix error

--- server/app.py
+++ server/app.py
@@ -36,8 +36,10 @@ def echo():
         image_array = base64_to_array(image_base64)
         result = get_predicted_number(image_array)
         response = jsonify({'prediction': str(result[0]), 'probability': str(result[1])})
+
+        return response
+
     
-    return response
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
