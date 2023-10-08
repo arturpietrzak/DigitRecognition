d60502e9769b5f34e82bb217544a6fbe62e184e8
arturpietrzak <artur.pietrzak1@outlook.com>
2023-10-08 02:18:37 +0200

    Rebuild frontend

--- server/app.py
+++ server/app.py
@@ -31,8 +31,8 @@ app = Flask(__name__, static_folder='frontend')
 @app.route('/classify_number', methods=['POST'])
 @cross_origin()
 def echo():
-    image_base64 = request.get_json()['image']
-    print(request.get_json())
+    image_base64 = request.json['image']
+    print(request.json)
     if (image_base64):
         image_array = base64_to_array(image_base64)
         result = get_predicted_number(image_array)
